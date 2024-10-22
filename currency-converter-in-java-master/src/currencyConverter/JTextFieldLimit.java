41a18c79ece3b8db7ccaf5dd0852c9c2a024e5b5
Sarah-m1 <sarahmazennoauralh3@gmail.com>
2024-10-23 01:34:49 +0300

    Update JTextFieldLimit.java

--- currency-converter-in-java-master/src/currencyConverter/JTextFieldLimit.java
+++ currency-converter-in-java-master/src/currencyConverter/JTextFieldLimit.java
@@ -1,32 +1,39 @@
-package currencyConverter;
+package currencyConverter; // Package of the project
 
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.PlainDocument;
+import javax.swing.text.AttributeSet; // Import AttributeSet interface to control text properties
+import javax.swing.text.BadLocationException; // Import BadLocationException for handling exceptions
+import javax.swing.text.PlainDocument; // Import PlainDocument to customize text documents
 
+// JTextFieldLimit class to set input limits in JTextField
 public class JTextFieldLimit extends PlainDocument {
-  private int limit;
-  private boolean toUppercase = false;
+  private int limit; // Maximum length for the input text
+  private boolean toUppercase = false; // Variable to determine if the text should be converted to uppercase
   
+  // Constructor that takes the length limit
   JTextFieldLimit(int limit) {
-   super();
-   this.limit = limit;
-   }
-   
+    super(); // Call the constructor of the superclass
+    this.limit = limit; // Set the limit
+  }
+  
+  // Another constructor that takes the length limit and an option for uppercase conversion
   JTextFieldLimit(int limit, boolean upper) {
-   super();
-   this.limit = limit;
-   toUppercase = upper;
-   }
+    super(); // Call the constructor of the superclass
+    this.limit = limit; // Set the limit
+    toUppercase = upper; // Set the option to convert text to uppercase
+  }
  
-  public void insertString
-    (int offset, String  str, AttributeSet attr)
+  // Method to insert text while checking its length
+  public void insertString(int offset, String str, AttributeSet attr)
       throws BadLocationException {
-   if (str == null) return;
+    // If the input string is null, do nothing
+    if (str == null) return;
 
-   if ((getLength() + str.length()) <= limit) {
-     if (toUppercase) str = str.toUpperCase();
-     super.insertString(offset, str, attr);
-     }
-   }
+    // Check if the new text length does not exceed the allowed limit
+    if ((getLength() + str.length()) <= limit) {
+      // If the option to convert text to uppercase is enabled, convert the string
+      if (toUppercase) str = str.toUpperCase();
+      // Call the insert method in the superclass to insert the text
+      super.insertString(offset, str, attr);
+    }
+  }
 }
