3c764c1e53169f0331b8a39e7cedd29b05cb77d4
Ruba63 <ror6314@gmail.com>
2024-10-23 15:35:02 +0300

    Update CurrencyConverter.java

--- currency-converter-in-java-master/src/currencyConverter/CurrencyConverter.java
+++ currency-converter-in-java-master/src/currencyConverter/CurrencyConverter.java
@@ -4,24 +4,25 @@ import java.awt.EventQueue;
 import javax.swing.UIManager;
 
 public class CurrencyConverter {
+    public static void main(String[] args) {
+        try {
+            // Set the look and feel of the application to match the system's look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Throwable e) {
+            e.printStackTrace(); // Print any exception to the stack trace for debugging
+        }
 
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					// Create and show main window at startup
-					MainWindow mainWindow = new MainWindow();
-					mainWindow.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+        // Use EventQueue to ensure the creation and showing of the main window occurs on the Event Dispatch Thread
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    // Create and show the main window at startup
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace(); // Print any exception to the stack trace for debugging
+                }
+            }
+        });
+    }
 }
